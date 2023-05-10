6a463295d39689d58bbbebc779acd91120e073fa
chaieeetanya433 <129309578+chaieeetanya433@users.noreply.github.com>
2023-05-10 09:41:27 +0530

    Delete modules directory

--- modules/6-alternative-flavour.js
+++
@@ -1,8 +0,0 @@
-module.exports.items = ['item1', 'item2']
-
-const person = {
-    name: 'bob',
-}
-
-//we're assigning some characterstic/properties(person) to object (singlePerson)
-module.exports.singlePerson = person
