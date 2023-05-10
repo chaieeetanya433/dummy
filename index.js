4e408cf444d8122aadf73732e8b3dbf89d8dee6f
chaieeetanya433 <129309578+chaieeetanya433@users.noreply.github.com>
2023-05-10 09:47:45 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-//importing contents of second.js to index.js 
-
-  
-    const chai = require("./second");
-    console.log("Hello world", chai)
-    
-/* o/p:-
-Hello world { name: 'chaitanya', favNum: 4, developer: true }
-and this type of import system is called as common js modules*/
